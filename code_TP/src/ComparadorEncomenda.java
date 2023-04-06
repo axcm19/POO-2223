24a00755ca6115a123a74b587594a58d0d56d476
axcm19 <afonsoxcm@gmail.com>
2023-04-06 20:03:58 +0100

    inicio do projeto

---
+++ code_TP/src/ComparadorEncomenda.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class ComparadorEncomenda implements Comparator<Encomenda> {
+
+    public int compare(Encomenda e1, Encomenda e2){
+        return e1.compareTo(e2);
+    }
+
+}
