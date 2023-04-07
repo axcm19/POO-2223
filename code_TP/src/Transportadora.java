66f86b9bc0f2b0f57df9238f36362af635a9533c
0xAbelheira <h.abelheira@gmail.com>
2023-04-07 01:59:02 +0100

    Classe Transportadora quase feita

--- code_TP/src/Transportadora.java
+++ code_TP/src/Transportadora.java
@@ -1,2 +1,117 @@
-public class Transportadora {
+/*
+ * Cada artigo terá uma transportadora atribuida (o vendedor é que
+ * decide que transportadora é que vai estar associcada a cada artigo)
+ */
+
+ public class Transportadora {
+    private String nomeTransportadora;
+    private double margemLucro;
+    private int imposto;
+    private int valorBase;
+    
+// -------------------- CONSTRUTORES --------------------
+
+// Construtor Vazio
+    public Transportadora(){
+        this.nomeTransportadora = " ";
+        // variável para cada transportadora
+        this.margemLucro = 0;
+        // imposto já deve estar pré-definido para cada transportadora (nao sei se precisa de ser uma variável de instância);
+        this.imposto = 0;
+        // para calcular este preco de expedição precisamos do valor base da encomenda e da margem de lucro e do impsosto.
+        // o problema aqui é que para saber o valor base da encomenda, precisamos de saber quantas encomendas é que são.
+        // ha um valor base diferente para cada tipo de encomenda:
+        // pequenas (1 artigo)
+        // médias (2 a 5 artigos)
+        // grandes (mais de 5 artigos)
+        this.valorBase = 0;
+        // portanto, temos que decidir onde é que esta quantidade de artigos vai ficar. na encomenda, ou aqui mesmo na transportadora?
+    }
+
+// Construtor Parametrizado
+    public Transportadora(String nomeTransportadora, double margemLucro, int imposto, int valorBase){
+        this.nomeTransportadora = nomeTransportadora;
+        this.margemLucro = margemLucro;
+        this.imposto = imposto;
+        this.valorBase = valorBase;
+    }
+
+// Construtor Copia
+    public Transportadora(Transportadora t){
+        this.nomeTransportadora = t.getNomeTransportadora();
+        this.margemLucro = t.getMargemLucro();
+        this.imposto = t.getImposto();
+        this.valorBase = t.getValorBase();
+    }
+
+// -------------------- GETTERS & SETTERS --------------------
+
+    private String getNomeTransportadora(){
+        return this.nomeTransportadora;
+    }
+
+    private void setNomeTransportadora(String nomeTransportadora) {
+        this.nomeTransportadora = nomeTransportadora;
+    }
+
+    private double getMargemLucro(){
+        return this.margemLucro;
+    }
+
+    private void setMargemLucro(int margemLucro){
+        this.margemLucro = margemLucro;
+    }
+
+    private int getImposto(){
+        return this.imposto;
+    }
+
+    private void setImposto(int imposto){
+        this.imposto = imposto;
+    }
+
+    private int getValorBase(){
+        return this.valorBase;
+    }
+
+    private void setValorBase(int valorBase){
+        this.valorBase = valorBase;
+    }
+
+//--------------------- CLONE / EQUALS ---------------------
+
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(o == null || o.getClass() != this.getClass()) return false;
+        Transportadora t = (Transportadora) o;
+        return (this.nomeTransportadora == t.getNomeTransportadora() 
+        && this.margemLucro == t.getMargemLucro()
+        && this.imposto == t.getImposto()
+        && this.valorBase == t.getValorBase()); 
+    }
+
+    public Transportadora clone(){
+        return new Transportadora(this);
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("TRANSPORTADORA: " + this.nomeTransportadora + "\n");
+        sb.append("Valor Expedição: " + calculaValorExpedicao(this));
+        String res = sb.toString();
+        return res;
+    }
+
+    public int compareTo(Transportadora t){
+        // comparação feita pelo nome da Transportadora 
+        return this.nomeTransportadora.compareTo(t.getNomeTransportadora());
+    }
+
+// -------------------- OUTROS MÉTODOS --------------------
+
+    public double calculaValorExpedicao(Transportadora t){
+        double precoExpedicao = (t.getValorBase() * t.getMargemLucro() * (1 + t.getImposto())) * 0.9;
+        return precoExpedicao;   
+    }
+
 }
