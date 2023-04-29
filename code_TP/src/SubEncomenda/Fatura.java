db227002d7bf2f04e2d5ed5e6f8ec73a5cac5e9a
0xAbelheira <h.abelheira@gmail.com>
2023-04-29 15:40:48 +0100

    Class invoice

--- code_TP/src/SubEncomenda/Fatura.java
+++ code_TP/src/SubEncomenda/Fatura.java
@@ -20,7 +20,6 @@ public class Fatura implements Serializable{
     private String nomeTransportadora;
     private String codArtigo;
 
-
     public Fatura() {
         this.faturaId = num_sequencia++;
         this.numeroEncomenda = 0;
@@ -52,7 +51,7 @@ public class Fatura implements Serializable{
     }
 
 
-    public void setFaturaId(int FaturaId) {
+    public void setFaturaId(int faturaId) {
         this.faturaId = faturaId;
     }
 
@@ -66,40 +65,81 @@ public class Fatura implements Serializable{
         this.faturaData = faturaData;
     }
 
-
-    public double getTotal() {
-        return total;
+    public int getNumeroEncomenda() {
+        return numeroEncomenda;
     }
 
-
-    public void setTotal(double total) {
-        this.total = total;
+    public void setNumeroEncomenda(int numeroEncomenda) {
+        this.numeroEncomenda = numeroEncomenda;
+    }
+    
+    public String getNomeVendedor() {
+        return nome_vendedor;
     }
 
-
-    public double getLucro() {
-        return this.lucro;
+    public void setNomeVendedor(String nome_vendedor) {
+        this.nome_vendedor = nome_vendedor;
     }
 
-
-    public void setLucro(double lucro) {
-        this.lucro = lucro;
+    public String getNifVendedor() {
+        return nif_vendedor;
     }
 
-
-    public Encomenda getEnc() {
-        return encomenda;
+    public void setNifVendedor(String nif_vendedor) {
+        this.nif_vendedor = nif_vendedor;
     }
 
-
-    public void setEnc(Encomenda enc) {
-        this.encomenda = enc;
+    public String getNomeComprador() {
+        return nome_comprador;
     }
 
+    public void setNomeComprador(String nome_comprador) {
+        this.nome_comprador = nome_comprador;
+    }
+    
+    public String getNifComprador() {
+        return nif_comprador;
+    }
+
+    public void setNifComprador(String nif_comprador) {
+        this.nif_comprador = nif_comprador;
+    }
+
+    public double getPreçoArtigo() {
+        return preço_artigo;
+    }
+
+    public void setPreçoArtigo(double preço_artigo) {
+        this.preço_artigo = preço_artigo;
+    }
+
+    public double getPreçoExpedicao() {
+        return preço_expedicao;
+    }
+
+    public void setPreçoExpedicao(double preço_expedicao) {
+        this.preço_expedicao = preço_expedicao;
+    }
+
+    public String getNomeTransportadora() {
+        return nomeTransportadora;
+    }
+
+    public void setNomeTransportadora(String nomeTransportadora) {
+        this.nomeTransportadora = nomeTransportadora;
+    }
+
+    public String getCodArtigo() {
+        return codArtigo;
+    }
+
+    public void setCodArtigo(String codArtigo) {
+        this.codArtigo = codArtigo;
+    }
 
     @Override
     public String toString() {
-        return "Invoice [ID = " + faturaId + ", Data de Emissão = " + faturaData + ", SubEncomenda.Encomenda = " + encomenda + ", Lucro = "
+        return "Fatura [ID = " + faturaId + ", Data de Emissão = " + faturaData + ", Encomenda = " + encomenda + ", Lucro = "
                 + lucro + ", Valor Total = " + total + "]";
     }
     
